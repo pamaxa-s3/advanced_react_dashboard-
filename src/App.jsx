4d913ecf6292626e422786f84171b006a972a0ea
Roman Strynzha <pamaxas3@gmail.com>
2026-01-05 21:50:32 +0200

    start project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from '@components/layout';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <h1>Analytics Dashboard</h1>
+    </>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    }
+  }
+});
